2404f8e1885739fccd46c331d836156e9b8e443c
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2022-12-19 11:35:24 +0200

    Delete utils directory

--- utils/tasks/copy.js
+++
@@ -1,1 +0,0 @@
-export const copy = () => app.gulp.src(app.path.src.files).pipe(app.gulp.dest(app.path.build.files));
--- utils/tasks/html.js
+++
@@ -1,6 +0,0 @@
-import fileInclude from "gulp-file-include";
-
-export const html = () => app.gulp
-    .src(app.path.src.html)
-    .pipe(fileInclude())
-    .pipe(app.gulp.dest(app.path.build.html));
