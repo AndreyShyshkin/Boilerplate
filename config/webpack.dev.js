600b2c9f1696f0b46176ab7403c029c7b79b3cd9
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-02-21 22:21:57 +0200

    Update webpack.dev.js

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,10 +7,12 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, '../src'),
+    static: {
+      directory: path.join(__dirname, '../src'),
+    },
     hot: true,
     port: 9000,
-    watchContentBase: true,
+    watchFiles: [path.join(__dirname, '../src/**/*')],
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 });
