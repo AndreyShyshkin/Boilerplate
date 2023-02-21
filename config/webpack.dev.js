78c21772e4ecd9b1a90d28e01856693aa0edb2b3
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-02-21 22:39:35 +0200

    Update webpack.dev.js

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,18 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, '../src'),
-    },
-    hot: true,
-    port: 9000,
-    watchFiles: [path.join(__dirname, '../src/**/*')],
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-});
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		static: {
+			directory: path.join(__dirname, '../src'),
+		},
+		hot: true,
+		port: 9000,
+		watchFiles: [path.join(__dirname, '../src/**/*')],
+		open: 'index.html',
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin()],
+})
