71530b314e31182483995ead442a64b9e809cb7e
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-02-27 00:12:46 +0200

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,5 @@
-const isDev = process.env.NODE_ENV === "development";
-
+const isDev = process.env.NODE_ENV === 'development'
 const environment = {
-  isDev,
-  apiUrl: isDev ? "http://localhost:3000" : "https://api.example.com",
-};
-
-module.exports = environment;
+	apiUrl: isDev ? 'http://localhost:3000' : 'https://api.example.com',
+}
+module.exports = environment
