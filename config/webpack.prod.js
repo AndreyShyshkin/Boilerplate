58079624ba713df877fcaff2f835bc9951b4db80
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-02-27 00:13:31 +0200

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,8 +2,11 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common')
 const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = merge(commonConfig, {
+module.exports = merge(common, {
 	mode: 'production',
+	output: {
+		filename: 'js/[name].[contenthash].js',
+	},
 	optimization: {
 		minimize: true,
 		minimizer: [new TerserPlugin()],
