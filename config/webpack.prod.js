be345d138918204a18d2ea15d2885c0e37f6e65d
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-02-27 23:55:46 +0200

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,9 +6,18 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(commonConfig, {
 	mode: 'production',
+	devtool: 'source-map',
 	optimization: {
 		minimize: true,
-		minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+		minimizer: [new TerserPlugin({
+			parallel: true,
+			terserOptions: {
+			  output: {
+			    comments: false,
+			  },
+			},
+			extractComments: false,
+	      }), new CssMinimizerPlugin()],
 	},
 	plugins: [new CleanWebpackPlugin()],
 })
